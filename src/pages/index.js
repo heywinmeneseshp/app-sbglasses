ab2abfaf3fc81b6803b58b6ca2942fb66196ed55
heywin <heywin1@gmail.com>
2022-04-12 17:20:17 -0500

    lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
 import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
 import Link from 'next/link';
+import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
-
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-];
 
 export default function Home() {
   return (
@@ -30,10 +23,10 @@ export default function Home() {
               <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
-                    <Link href="/login">
+                    <div href="/login">
                       <span className="sr-only">Workflow</span>
                       <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" />
-                    </Link>
+                    </div>
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Open main menu</span>
@@ -43,11 +36,6 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                  {navigation.map((item) => (
-                    <Link key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                      {item.name}
-                    </Link>
-                  ))}
                   <Link href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
                     Log in
                   </Link>
@@ -77,16 +65,7 @@ export default function Home() {
                       </Popover.Button>
                     </div>
                   </div>
-                  <div className="px-2 pt-2 pb-3 space-y-1">
-                    {navigation.map((item) => (
-                      <Link key={item.name} href={item.href} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
-                  <Link href="/login" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
-                    Log in
-                  </Link>
+                  <div className="px-2 pt-2 pb-3 space-y-1"></div>
                 </div>
               </Popover.Panel>
             </Transition>
@@ -102,20 +81,20 @@ export default function Home() {
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <Link
+                  <div
                     href="/login"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
-                  </Link>
+                  </div>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link
+                  <div
                     href="/login"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Live demo
-                  </Link>
+                  </div>
                 </div>
               </div>
             </div>
